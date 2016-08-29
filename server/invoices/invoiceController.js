f8cf592b5a8dcb1309d978cd3ea9a336e4655085
Tim Z <tjkz87@gmail.com>
2016-08-29 11:29:37 -0700

    Moved logic from invoiceController to invoiceSoap

--- server/invoices/invoiceController.js
+++ server/invoices/invoiceController.js
@@ -1,66 +1,16 @@
-var soap = require('soap');
 var soapClient = require('./invoiceSoap');
-
-var client;
-
-soapClient(function(cl) {
-  client = cl;
-  console.log('InvoiceService SOAP client loaded.')
-});
 
 module.exports = {
   query: function(req, res, next) {
-    // call SOAP API
-    // ---------- Request body example:----------
-    // order is important!
-    // {
-    //   "sessionId": "[ESF username (IIN or BIN)]",
-    //   "criteria": {
-    //     "direction": "[INBOUND or OUTBOUND]", //required
-    //     "contragentTin": "[BIN or IIN]",
-    //     "dateFrom": [timestamp],
-    //     "dateTo": [timestamp],
-    //     "invoiceStatus": [],
-    //     "invoiceType": [],
-    //     "orderBy": [],
-    //     "asc": [true or false],  //required
-    //     "pageNum": []
-    //   }
-    // }
-    // ------------------------------------------
-    var queryParams = {
-      sessionId: req.query.sessionId,
-      criteria: {
-        direction: req.query.direction
-      }
-    }
-
-    if (req.query.contragentTin) { queryParams.criteria.contragentTin = req.query.contragentTin }
-    if (req.query.dateFrom) { queryParams.criteria.dateFrom = req.query.dateFrom }
-    if (req.query.dateTo) { queryParams.criteria.dateTo = req.query.dateTo }
-    if (req.query.invoiceStatus) { queryParams.criteria.invoiceStatus = req.query.invoiceStatus }
-    if (req.query.invoiceType) { queryParams.criteria.invoiceType = req.query.invoiceType }
-    if (req.query.orderBy) { queryParams.criteria.orderBy = req.query.orderBy }
-    queryParams.criteria.asc = req.query.asc;
-    if (req.query.pageNum) { queryParams.criteria.pageNum = req.query.pageNum }
-
-    client.queryInvoice(queryParams, function(err, result) {
-      if (err) { return res.send(err); }
+    soapClient.queryInvoice(req.query, function(err, result) {
+      if (err) { return res.send(err) }
       res.send(result);
-    }, {rejectUnauthorized: false});
+    });
   },
   queryById: function(req, res, next) {
-    console.log(req.query.idList);
-    var queryParams = {
-      sessionId: req.query.sessionId,
-      idList: {
-        id: req.query.idList
-      }
-    }
-    client.queryInvoiceById(queryParams, function(err, result) {
-      if (err) { return res.send(err); }
+    soapClient.queryInvoiceById(req.query, function(err, result) {
+      if (err) { return res.send(err) }
       res.send(result);
-    }, {rejectUnauthorized: false});
-    //res.send('queryById!');
+    });
   }
 }
--- server/invoices/invoiceSoap.js
+++ server/invoices/invoiceSoap.js
@@ -15,9 +15,67 @@ var wsdlOptions = {
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
-module.exports = function(callback) {
-  soap.createClient(wsdl, wsdlOptions, function(err, client) {
-    if (err) { throw err; }
-    callback(client);
-  });
+var client;
+
+soap.createClient(wsdl, wsdlOptions, function(err, cl) {
+  if (err) { throw err; }
+  client = cl;
+  console.log('InvoiceService SOAP client loaded.')
+});
+
+module.exports = {
+  queryInvoice: function(params, callback) {
+    // organize query params
+    // call SOAP API
+    // ---------- Request body example:----------
+    // order is important!
+    // {
+    //   "sessionId": "[ESF username (IIN or BIN)]",
+    //   "criteria": {
+    //     "direction": "[INBOUND or OUTBOUND]", //required
+    //     "contragentTin": "[BIN or IIN]",
+    //     "dateFrom": [timestamp],
+    //     "dateTo": [timestamp],
+    //     "invoiceStatus": [],
+    //     "invoiceType": [],
+    //     "orderBy": [],
+    //     "asc": [true or false],  //required
+    //     "pageNum": []
+    //   }
+    // }
+    // ------------------------------------------
+    var queryParams = {
+      sessionId: params.sessionId,
+      criteria: {
+        direction: params.direction
+      }
+    }
+    if (params.contragentTin) { queryParams.criteria.contragentTin = params.contragentTin }
+    if (params.dateFrom) { queryParams.criteria.dateFrom = params.dateFrom }
+    if (params.dateTo) { queryParams.criteria.dateTo = params.dateTo }
+    if (params.invoiceStatus) { queryParams.criteria.invoiceStatus = params.invoiceStatus }
+    if (params.invoiceType) { queryParams.criteria.invoiceType = params.invoiceType }
+    if (params.orderBy) { queryParams.criteria.orderBy = params.orderBy }
+    queryParams.criteria.asc = params.asc;
+    if (params.pageNum) { queryParams.criteria.pageNum = params.pageNum }
+    // call SOAP 
+    client.queryInvoice(queryParams, function(err, result) {
+      if (err) { return callback(err, null); }
+      callback(null, result);
+    }, {rejectUnauthorized: false});
+  },
+  queryInvoiceById: function(params, callback) {
+    // organize query params
+    var queryParams = {
+      sessionId: params.sessionId,
+      idList: {
+        id: params.idList
+      }
+    }
+    // call SOAP
+    client.queryInvoiceById(queryParams, function(err, result) {
+      if (err) { return callback(err, null); }
+      callback(null, result);
+    }, {rejectUnauthorized: false});
+  }
 }
