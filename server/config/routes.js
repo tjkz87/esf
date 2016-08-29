0ab463508a813ccebe11d41d072abf2f795ca260
Tim Z <tjkz87@gmail.com>
2016-08-28 18:54:21 -0700

    Added api/createsession POST handler

--- server/config/routes.js
+++ server/config/routes.js
@@ -2,4 +2,5 @@ var userController = require('../users/userController');
 
 module.exports = function(app, express) {
   app.post('/api/users/info', userController.getInfo);
+  app.post('/api/users/session', userController.createSession);
 };
--- server/users/userController.js
+++ server/users/userController.js
@@ -1,34 +1,45 @@
 var soap = require('soap');
 var soapClient = require('./userSoap');
 
+var client;
+
+soapClient(function(cl) {
+  client = cl;
+});
+
 module.exports = {
   getInfo: function(req, res, next) {
-    // Request body example:
+    // ---------- Request body example:----------
     // {
-    // "username": "[ESF username (IIN)]",
+    // "username": "[ESF username (IIN or BIN)]",
     // "password": "[ESF password]",
     // "data": {
-    //     "tin": "[use IIN]",
+    //     "tin": "[use IIN or BIN]",
     //     "x509Certificate": "[user's public certificate (PEM)]"
     //   }
     // }
-    soapClient(function(client) {
-      // set security headers
-      client.setSecurity(new soap.WSSecurity(req.body.username, req.body.password, {
-        hasTimeStamp: false,
-        hasTokenCreated: false
-      }));
-      // call SOAP API
-      client.SessionService.SessionServicePort.getUser(req.body.data, function(err, result) {
-        if (err) {
-          res.send(err);
-        } else {
-          res.send(result);
-        }
-      }, {rejectUnauthorized: false});
-    });
+    // ------------------------------------------
+    // set security headers
+    client.setSecurity(new soap.WSSecurity(req.body.username, req.body.password, {
+      hasTimeStamp: false,
+      hasTokenCreated: false
+    }));
+    // call SOAP API
+    client.SessionService.SessionServicePort.getUser(req.body.data, function(err, result) {
+      if (err) { return res.send(err); }
+      res.send(result);
+    }, {rejectUnauthorized: false});
   },
   createSession: function(req, res, next) {
-
+    // set security headers
+    client.setSecurity(new soap.WSSecurity(req.body.username, req.body.password, {
+      hasTimeStamp: false,
+      hasTokenCreated: false
+    }));
+    // call SOAP API
+    client.SessionService.SessionServicePort.createSession(req.body.data, function(err, result) {
+      if (err) { return res.send(err); }
+      res.send(result);
+    }, {rejectUnauthorized: false});
   }
 }
